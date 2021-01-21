d470b8fee8175c4cc77a3cf1e7ca69f022349b3d
wnmMrrd <51522734+wnmMrrd@users.noreply.github.com>
2021-01-21 09:20:12 +0800

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,6 +1,4 @@
-# this script is called when the judge is building your compiler.
-# no argument will be passed in.
+set ff=UNIX
 set -e
-cd "$(dirname "$0")"
 mkdir -p bin
-find ./src -name *.java | javac -d bin -classpath "lib/antlr-4.9.1-complete.jar" @/dev/stdin
+find ./src -name *.java | javac -d bin -cp /ulib/java/antlr-4.9-complete.jar @/dev/stdin
